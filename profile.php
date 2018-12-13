5435125cf81913000d19b9740b393076309282b1
Mpr1983 <30472031+Mpr1983@users.noreply.github.com>
2018-12-13 10:57:41 +0100

    Test Build

--- profile.php
+++ profile.php
@@ -11,7 +11,7 @@
     http://www.osticket.com
 
     Released under the GNU General Public License WITHOUT ANY WARRANTY.
-    See LICENSE.TXT for details.
+    See LICENSE.TXT for details
 
     vim: expandtab sw=4 ts=4 sts=4:
     $Id: $
